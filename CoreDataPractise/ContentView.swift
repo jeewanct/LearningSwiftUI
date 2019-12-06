dd566fe74022d0fecb2f6d180b9d275ce20c2342
Jeevan <jeevan.tiwari@one97.net>
2019-12-06 18:10:03 +0530

    First commit

---
+++ CoreDataPractise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataPractise
+//
+//  Created by jeevan tiwari on 21/11/19.
+//  Copyright © 2019 jeevan tiwari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
